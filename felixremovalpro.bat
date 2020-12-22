ac276db08296c83fe43c7f2555d05356f0d93481
felixhume <50804992+felixhume@users.noreply.github.com>
2020-12-22 12:32:34 -0800

    Add files via upload

---
+++ felixremovalpro.bat
@@ -0,0 +1,13 @@
+@echo off
+color 70
+echo Felix Removal PRO v1
+echo Made by Felix Hume
+pause
+echo Thanks for choosing Felix Removal Pro
+echo We will walk you through the steps to tell Windows to shut up.
+pause
+echo Keep this on your computer for further use
+echo You need to restart your computer for this to take effect.
+taskkill /F /IM explorer.exe
+explorer.exe
+exit
